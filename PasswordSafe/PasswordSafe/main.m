1138822fc4a09346d6af7e0ef505fadc39781112
Jacob Schuenke <JakeSchuenke@gmail.com>
2013-01-24 21:26:22 -0500

    Fixed the compile errors and warnings.  Clean start to the code.

--- PasswordSafe/PasswordSafe/main.m
+++ PasswordSafe/PasswordSafe/main.m
@@ -15,17 +15,5 @@
 
 int main(int argc, char *argv[])
 {
-    Generator *test = [[Generator alloc] init];
-    NSMutableString *testString = [[NSMutableString alloc] init];
-//    NSMutableString *aTitle = [[NSMutableString alloc] initWithString:@"titulars"];
-    for (int i = 0; i < 5; i++) {
-        [testString setString:[test generatePassword:10 :5 :3 :0 :0 :TRUE :TRUE :FALSE :TRUE]];
-    NSLog(testString);
-    }
-//    [testString setString:[test generatePassword:8 :1 :3 :2 :2]];
-//    Note *testNote = [[Note alloc] initWithValues: aTitle: testString];
-//    [testNote saveNote];
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+
 }
