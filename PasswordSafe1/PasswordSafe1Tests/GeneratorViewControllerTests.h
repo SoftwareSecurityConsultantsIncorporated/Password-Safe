79dc420031ed78514af7df12dd4d93eb1074f5ef
Jacob Schuenke <JakeSchuenke@gmail.com>
2013-03-28 02:49:43 -0400

    added Generator Screen Automation Logs

---
+++ PasswordSafe1/PasswordSafe1Tests/GeneratorViewControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  GeneratorViewControllerTests.h
+//  PasswordSafe1
+//
+//  Created by CSSE Department on 3/27/13.
+//  Copyright (c) 2013 Software Security Consultants Incorporated. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GeneratorViewControllerTests : SenTestCase
+
+@end
---
+++ PasswordSafe1/PasswordSafe1Tests/GeneratorViewControllerTests.m
@@ -0,0 +1,13 @@
+//
+//  GeneratorViewControllerTests.m
+//  PasswordSafe1
+//
+//  Created by CSSE Department on 3/27/13.
+//  Copyright (c) 2013 Software Security Consultants Incorporated. All rights reserved.
+//
+
+#import "GeneratorViewControllerTests.h"
+
+@implementation GeneratorViewControllerTests
+
+@end
